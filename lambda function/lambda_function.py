abbf1095e94461d46bcdc1f703629b1b7d0b6411
Matar Ravitz <128381233+matarravitz@users.noreply.github.com>
2025-01-06 19:52:53 +0200

    Update lambda_function.py
    need to fix get_cache function
    and finish document the main function.

--- lambda function/lambda_function.py
+++ lambda function/lambda_function.py
@@ -16,7 +16,16 @@ table = dynamodb.Table('Domain-to-emails')
 
 
 def lambda_response(response_dict: Dict, status_code: int = 200) -> Dict:
-    # raise the appropriate error message
+    """
+    Raise the appropriate message - error or success.
+
+    Args:
+        response_dict (Dict): Dictionary that contains the response's details.
+        status_code (int, optional): HTTP status code. Defaults to 200 - success.
+
+    Returns:
+        Dict: Dictionary formatted as an HTTP Lambda response object with CORS headers.
+    """
     return {
         "isBase64Encoded": False,
         "statusCode": status_code,
@@ -29,6 +38,15 @@ def lambda_response(response_dict: Dict, status_code: int = 200) -> Dict:
 
 
 def email_extractor(url_response: str) -> Set[str]:
+    """
+    Extract all valid email addresses from a given HTML source.
+
+    Args:
+        url_response (str): The HTML content to extract emails from.
+
+    Returns:
+        Set[str]: A set of unique and valid email addresses that was found.
+    """
     emails = set()
     match_emails = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', url_response)
     # Using regex to find all strings that match the email pattern.
@@ -40,37 +58,65 @@ def email_extractor(url_response: str) -> Set[str]:
 
 
 def url_extractor(url_response: str, url: str) -> Set[str]:
+    """
+    Extract all valid url addresses from an HTML source.
+
+    Args:
+        url_response (str): The HTML content to extract emails from.
+        url (str): The URL to be used as the base for the search and extraction process.
+
+    Returns:
+        Set[str]: A set of unique and valid url addresses that was found.
+    """
     urls = set()
     soup = BeautifulSoup(url_response, "html.parser")
-    links = soup.find_all('a', href=True)
-    domain = urlparse(url).netloc
+    links = soup.find_all('a', href=True) # finds all the lines in the HTML content that mark with <a> tag and not umpty.
+    domain = urlparse(url).netloc # extracts the url's domain
     for link in links:
-        href = link['href']
+        href = link['href'] # extract the href attribute from an HTML tag.
         extracted_url = urljoin(url, href)
         if validators.url(extracted_url) and urlparse(extracted_url).netloc == domain:
             if extracted_url != url and extracted_url != url + "/" and \
             "#" not in extracted_url and extracted_url not in urls:
+                #The check for "#" not in extracted_url is included because URLs containing the # character are often reference a specific section within the same page, rather than a separate page.
                 urls.add(extracted_url)
-        #if len(urls) >= 50:
-            #break
+        # Ensures that the links added valid and belong to the same domain as the given URL.  
+        # Ensures that the links added are uniqe.
     return urls
 
 
-async def web_scanner(url: str, session: ClientSession, num_of_round) -> Dict:
+async def web_scanner(url: str, session: ClientSession, int: num_of_round) -> Dict[str, Set[str]]:
+    """
+    An asynchronous function that extracts valid URL and email addresses from an HTML source.
+
+    Args:
+        url (str): The base URL for the search and extraction process.
+        session (ClientSession): A shared HTTP client session used for making requests efficiently across multiple scans.
+        num_of_round (int): The current round number of the scan.
+
+    Returns:
+        Dict[str, Set[str]]: A dictionary containing sets of unique and valid URLs and email addresses found.
+    """
     urls = set()
-    async with session.get(url) as response:
+    async with session.get(url) as response: ##########לוודא עם בובי שהבנתי###########################################
         response = await response.text()
     # get the source code.
     emails = email_extractor(response)
-    if num_of_round == 1:
+    if num_of_round == 1: # if it's the first round, starting a second acan
         urls = url_extractor(response, url)
     # extracting all the emails and urls from the source code.
     return {"url": url, "emails": emails, "paths": urls}
 
 
 def cache_emails(scan_results: List[Dict]) -> None:
-    # entering data to the dynamoDB table.
-    for i in range(0, len(scan_results), 100):
+    """
+    Saves the extracted emails and URLs from the base URL in the dynamoDB table for faster extraction in the future.
+
+    Args:
+        scan_results (List[Dict]): A list of dictionaries containing the extracted emails and URLs to be cached.
+    """
+    
+    for i in range(0, len(scan_results), 100): #making chankes of size 100 
         if len(scan_results) - i >= 100:
             chunk = scan_results[i:i + 100]
         else:
@@ -81,7 +127,7 @@ def cache_emails(scan_results: List[Dict]) -> None:
         with table.batch_writer() as batch:
             for result in chunk:
                 url = result['url']
-                if url in urls:
+                if url in urls: # if there is already the same url # TODO: check if needed.
                     continue
                 urls.append(url)
                 email_list = result["emails"]
@@ -94,7 +140,7 @@ def cache_emails(scan_results: List[Dict]) -> None:
                 })
 
 
-def get_cache(urls: Set[str]) -> Dict:
+def get_cache(urls: Set[str]) -> Dict: #TODO: need to be fixed!
     results = {}
     urls = list(urls)
     for i in range(0, len(urls), 100):
@@ -118,24 +164,46 @@ def get_cache(urls: Set[str]) -> Dict:
 
 
 async def controller(url: str, session: ClientSession, response_dynamo, num_of_round) -> Dict:
+    """
+    Controls the scanning of email addresses and URLs for a given base URL.
+    This function checks if the given URL is cached in the DynamoDB response. If cached, it retrieves the data
+    from the cache. Otherwise, it performs a scan of the URL using `web_scanner`, saves the results, and returns them.
+    It also handles timeout errors based on the scan round.
+
+    Args:
+        url (str): The base URL for the search and extraction process.
+        session (ClientSession): A shared HTTP client session used for making requests efficiently across multiple scans.
+        response_dynamo (dict): A dictionary containing cached results for previously scanned URLs.
+        num_of_round (int): The current scan round number, used to determine retry logic.
+    
+    Returns:
+        Dict: A dictionary containing the URL, email addresses, and paths. If the scan fails during the second round, 
+        an empty dictionary is returned.
+
+    Raises:
+        TimeoutError: Raised if a timeout occurs during the first round of scanning.
+    """
     try:
         if url not in response_dynamo:
-            # if not in cache, scan it, and save the data in the database.
+            # If the URL is not found in the cache, perform a scan using the web_scanner function
+            # and save the scanned data in the database.
             result = await web_scanner(url, session, num_of_round)
 
         else:
+            # If the URL is already scanned and cached, retrieve the data from the cache.
             result = {
                 "url": url,
                 "emails": response_dynamo[url]['email_list'],
                 "paths": response_dynamo[url]['path_list']
             }
-            # if the url were scanned, using the saved data.
         return result
 
     except TimeoutError :
         if num_of_round == 1:
+            # If a timeout occurs during the first scan round, raise a TimeoutError
             raise TimeoutError
         if num_of_round == 2:
+            #  If a timeout occurs during the second scan round, return an empty dictionary instead of raising an error.
             return dict()
 
 
@@ -192,6 +260,7 @@ async def main(event, context) -> Dict[str, Any]:
 
 
 def lambda_handler(event, context) -> Dict[str, Any]:
+    # Wrapper Function (lambda can't be asynchronous)
     return asyncio.run(main(event, context))
 
 
